0f44cff875fc2274a71fa2f4f7c78bea033fad9c
stratossalpas <stratossalpas@aueb.gr>
2025-05-14 20:29:19 +0300

    feat:add aaa

--- src/gr/aueb/cf/helloworld/AddApp.java
+++ src/gr/aueb/cf/helloworld/AddApp.java
@@ -3,7 +3,7 @@ package gr.aueb.cf.helloworld;
 public class AddApp {
     public static void main(String[] args) {
         System.out.println(add(3,5));
-
+        System.out.println("aaaa");
     }
     public static int add(int a,int b) {
         return a + b;
